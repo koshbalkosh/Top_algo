3e57bc6fb20c57fe25add48903736fe8a1f79b65
Koshin <163148836+koshbalkosh@users.noreply.github.com>
2024-06-15 22:30:23 +0300

    Update palindrom.py

--- palindrom.py
+++ palindrom.py
@@ -5,3 +5,18 @@ if s == s[::-1]:
   print("Это палиндром")
 else:
   print("Это не палиндром")
+
+
+# 2. Метод с использованием цикла
+
+s = "radar"
+is_palindrome = True
+for i in range(len(s) // 2):
+  if s[i] != s[-i - 1]:
+    is_palindrome = False
+    break
+
+if is_palindrome:
+  print("Это палиндром")
+else:
+  print("Это не палиндром")
