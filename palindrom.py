887113941029d4e7711f30899869196cfdb9fe7d
Koshin <163148836+koshbalkosh@users.noreply.github.com>
2024-06-17 15:05:50 +0300

    Update palindrom.py

--- palindrom.py
+++ palindrom.py
@@ -20,3 +20,13 @@ if is_palindrome:
   print("Это палиндром")
 else:
   print("Это не палиндром")
+
+
+# 3. Использование функции
+
+def is_palindrome(s):
+  if len(s) < 2:
+    return True
+  if s[0] != s[-1]:
+    return False
+  return is_palindrome(s[1:-1])
