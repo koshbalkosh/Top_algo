1142c8a52d74bb7a4498b42561fda77340814967
Koshin <163148836+koshbalkosh@users.noreply.github.com>
2024-06-30 16:42:32 +0300

    Update 2 anagram.py

--- anagram.py
+++ anagram.py
@@ -1,4 +1,4 @@
-# Способ 1
+# 1. Способ 1
 
 def is_anagram(str1, str2):
   # создаем словарь для первого слова
@@ -13,3 +13,24 @@ def is_anagram(str1, str2):
  
   # сравниваем словари
   return dict1 == dict2
+
+
+# 2. Оптимизация алгоритма
+
+def is_anagram(str1, str2):
+  # создаем словарь для первого слова
+  dict1 = {}
+  for char in str1:
+    dict1[char] = dict1.get(char, 0) + 1
+ 
+  # проверяем второе слово
+  for char in str2:
+    if char in dict1:
+      dict1[char] -= 1
+      if dict1[char] == 0:
+        del dict1[char]
+    else:
+      return False
+ 
+  # если словарь пуст, значит слова являются анаграммами
+  return not dict1
